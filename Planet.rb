f7041c69e3137b152c90db0dac6a4d7cce5923c4
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-11-29 13:24:08 -0700

    Update Planet class

--- Planet.rb
+++ Planet.rb
@@ -1,14 +1,16 @@
 class Planet
 
-  def initialize(image, mass, x, y)
+  def initialize(x, y, xvel, yvel, mass, image)
     @image = Gosu::Image.new(image)
     @mass = mass
     @x = x
     @y = y
+    @xvel = xvel
+    @yvel = yvel
   end
 
   def draw
     @image.draw(@x, @y, ZOrder::Planet)
   end
-  
+
 end
--- nbody.rb
+++ nbody.rb
@@ -8,8 +8,11 @@ class NbodySimulation < Gosu::Window
     super(640, 640, false)
     self.caption = "NBody simulation"
     @background_image = Gosu::Image.new("images/space.jpg", tileable: true)
-    @mercury = Planet.new("images/mercury.png", 0, 320, 400)
-    @venus = Planet.new("images/venus.png", 0, 320, 320)
+    @sun = Planet.new("images/sun.png", 0, 320, 320)
+    @mercury = Planet.new("images/mercury.png", 0, 320, 262)
+    @venus = Planet.new("images/venus.png", 0, 320, 212)
+    @earth = Planet.new("images/earth.png", 0, 320, 170)
+    @mars = Planet.new("images/mars.png", 0, 320, 92)
   end
 
   def update
@@ -17,8 +20,11 @@ class NbodySimulation < Gosu::Window
 
   def draw
     @background_image.draw(0, 0, ZOrder::Background)
+    @sun.draw
     @mercury.draw
     @venus.draw
+    @earth.draw
+    @mars.draw
   end
 
   def button_down(id)
@@ -29,3 +35,5 @@ end
 
 window = NbodySimulation.new
 window.show
+
+#to-do: redo arguments, create a method to read simulations, scale stuff
